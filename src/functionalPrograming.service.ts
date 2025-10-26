09503336223bbbfef1dd2cbb721531b2c24a647c
Weeraphat2000 <weeraphat.main@gmail.com>
2025-10-27 00:12:52 +0700

    update functional programming

--- src/functionalPrograming.service.ts
+++ src/functionalPrograming.service.ts
@@ -1,37 +1,80 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class FunctionalProgrammingServiceMath {
-  doSomethingFunction(x: number, ...func: ((x: number) => number)[]): number {
-    for (const f of func) {
-      x = f(x);
-    }
-    return x;
-    // return func.reduce((acc, currFunc) => currFunc(acc), x);
-  }
+  // higher-order function
+  plusBy =
+    (x: number): ((y: number) => number) =>
+    (y: number): number =>
+      x + y;
 
-  addFive(x: number): number {
-    return x + 5;
-  }
+  // higher-order function
+  divideBy =
+    (divisor: number) =>
+    (x: number): number =>
+      divisor === 0
+        ? (() => {
+            throw new BadRequestException('Division by zero');
+          })()
+        : x / divisor;
 
-  divideByTwo(x: number): number {
-    return x / 2;
-  }
+  //   divideBy =
+  //     (divisor: number) =>
+  //     (x: number): number => {
+  //       if (divisor === 0)
+  //         throw new BadRequestException('Division by zero is not allowed.');
+  //       return x / divisor;
+  //     };
 
-  multiplyByThree(x: number): number {
-    return x * 3;
-  }
+  // higher-order function
+  multiplyBy =
+    (factor: number) =>
+    (x: number): number =>
+      x * factor;
 
+  isEven = (x: number): boolean => x % 2 === 0;
+
+  // ฟังก์ชันสำหรับ compose ฟังก์ชันหลายตัวเข้าด้วยกัน
+  compose =
+    <T>(...fns: Array<(x: T) => T>) =>
+    (x: T): T =>
+      fns.reduce((acc, fn) => fn(acc), x);
+
+  //   compose = (...func: ((x: number) => number)[]): ((x: number) => number) => {
+  //     return (x: number): number => {
+  //       let result = x;
+  //       for (const f of func) {
+  //         result = f(result);
+  //       }
+  //       return result;
+  //     };
+  //     // return func.reduce(
+  //     //   (f, g) =>
+  //     //     (x: number): number =>
+  //     //       g(f(x)),
+  //     // );
+  //   };
+
+  // higher-order function
+  apply =
+    (fn: (x: number) => number) =>
+    (factor: number): number =>
+      fn(factor);
+  addFive = (x: number): number => x + 5;
+  addTen = (x: number): number => x + 10;
+
+  // ใช้ composition เพื่อสร้าง pipeline ทางคณิตศาสตร์
   get(math: number): number {
-    return this.doSomethingFunction(
-      math,
-      this.divideByTwo,
-      this.addFive,
-      this.addFive,
-      this.addFive,
-      this.divideByTwo,
-      this.multiplyByThree,
-      this.multiplyByThree,
+    const pipeline = this.compose(
+      this.plusBy(10),
+      this.divideBy(2),
+      this.divideBy(3),
+      this.multiplyBy(3),
+      this.multiplyBy(2),
+      this.apply(this.addFive),
+      this.apply(this.addTen),
     );
+
+    return pipeline(math);
   }
 }
